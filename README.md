ffe5599ba3670770ca13826471d902d7af50768b
basedmarv <sabasmarvinn@gmail.com>
2024-07-24 00:31:05 -0400

    Create spring project

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# Project-1
-Inventory Management Project
+# Project 1 - Inventory Management
+
+This is my full-stack Inventory Management solution consisting of:
+- Java with Spring Boot
+- PostgreSQL
+- React
---
+++ inventory-mgmt/src/main/java/com/skillstorm/inventory_mgmt/InventoryMgmtApplication.java
@@ -0,0 +1,13 @@
+package com.skillstorm.inventory_mgmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryMgmtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryMgmtApplication.class, args);
+	}
+
+}
