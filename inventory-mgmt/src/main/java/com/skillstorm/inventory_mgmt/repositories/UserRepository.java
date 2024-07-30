723570ec5d8883c3ff3b34783c5186ba73a7847d
basedmarv <sabasmarvinn@gmail.com>
2024-07-30 15:34:32 -0400

    Remove user entity from ER diagram

--- inventory-mgmt/src/main/java/com/skillstorm/inventory_mgmt/repositories/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.skillstorm.inventory_mgmt.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.skillstorm.inventory_mgmt.models.User;
-
-public interface UserRepository extends JpaRepository<User, Integer>{
-    
-}
