ec74adf3118db9ab87a249b6f8fcb0d8d6d72c8e
basedmarv <sabasmarvinn@gmail.com>
2024-10-11 12:21:28 -0400

    Create ItemServiceTest & WarehouseServiceTest

---
+++ inventory-mgmt/src/test/java/com/skillstorm/inventory_mgmt/services/ItemServiceTest.java
@@ -0,0 +1,143 @@
+package com.skillstorm.inventory_mgmt.services;
+
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import com.skillstorm.inventory_mgmt.models.Item;
+import com.skillstorm.inventory_mgmt.repositories.ItemRepository;
+
+public class ItemServiceTest {
+     //creates a mock instance of the ItemRepository class
+    @Mock
+    private ItemRepository itemRepository;
+
+    //injects the ItemRepository mock object into the ItemService
+    @InjectMocks
+    private ItemService itemService;
+    private AutoCloseable closeable;
+
+    //initializes the mock objects to ensure they are ready before tests are run
+    @BeforeTest
+    public void setup() {
+        closeable = MockitoAnnotations.openMocks(this);
+    }
+
+    //closes the closeable resource to ensure that the mock objects are cleaned up properly - prevents memory leaks
+    @AfterTest
+    public void teardown() throws Exception{
+        closeable.close();
+    }
+
+    // Tests if the mocked item repo successfully return expected response
+    // after service calls findAll() with expected values
+    @Test
+    public void findAllItemsTest() {
+        //Given
+        List<Item> expectedItems = Arrays.asList(new Item(), new Item());
+
+        //When (stubbing)
+        when(itemRepository.findAll())
+        
+        //Then
+        .thenReturn(expectedItems);
+
+        List<Item> response = itemService.findAll();
+
+        Assert.assertEquals(response, expectedItems);
+    }
+
+    // Tests if the mocked item repo successfully return expected response
+    // after service calls findById() for a item id that does exist 
+    @Test
+    public void findItemByIdTest() {
+        int itemId = 1;
+        Item inputItem = new Item();
+        Optional<Item> expectedItem = Optional.of(inputItem);
+        
+        when(itemRepository.findById(itemId))
+        .thenReturn(expectedItem);
+
+        Optional<Item> response = itemService.findById(itemId);
+
+        Assert.assertEquals(response, expectedItem);
+    }
+
+    // Tests if the mocked item repo successfully return expected response
+    // after service calls save() to create a item
+    @Test
+    public void createItemTest() {
+        Item inputItem = new Item();
+        Item savedItem = new Item();
+
+        when(itemRepository.save(inputItem))
+        .thenReturn(savedItem);
+
+        Item response = itemService.save(inputItem);
+
+        Assert.assertEquals(response, savedItem);
+    }
+
+    // Tests if the mocked item repo successfully return expected response
+    // after service calls update()
+    @Test
+    public void updateItemTest() {
+        Item inputItem = new Item(1, "itemName", "itemDescription", 30,10);
+        Item savedItem =  new Item(1, "itemName", "itemDescription", 30,10);
+
+        when(itemRepository.save(inputItem))
+        .thenReturn(savedItem);
+
+        when(itemRepository.existsById(inputItem.getItemId()))
+        .thenReturn(true);
+        
+        System.out.println(savedItem);
+
+        int response = itemService.updateItemById(inputItem.getItemId(), inputItem);
+
+        Assert.assertEquals(response, inputItem.getItemId());
+    }
+
+    // Tests if the mocked item repo successfully return expected EXCEPTION
+    // after service calls update()
+    @Test
+    public void updateItemInvalidTest() {
+        Item inputItem = new Item(1, "itemName", "itemDescription", 30,10);
+
+        when(itemRepository.save(inputItem))
+        .thenReturn(inputItem);
+
+        when(itemRepository.existsById(2))
+        .thenReturn(false);
+        
+        Assert.assertThrows(NoSuchElementException.class, () -> {
+            itemService.updateItemById(2, inputItem);
+        });
+    }
+
+    // Tests if the mocked itemInventory repo successfully deletes item with matching id after service calls deleteById()
+    @Test
+    public void deleteItemTest() {
+        Item inputItem = new Item(1, "itemName", "itemDescription", 30,10);
+        Optional<Item> expectedItem = Optional.of(inputItem);
+
+        when(itemRepository.findById(inputItem.getItemId()))
+        .thenReturn(expectedItem);
+        
+        itemRepository.deleteById(inputItem.getItemId());
+        verify(itemRepository, times(1)).deleteById(inputItem.getItemId());
+    }
+}
---
+++ inventory-mgmt/src/test/java/com/skillstorm/inventory_mgmt/services/WarehouseServiceTest.java
@@ -0,0 +1,140 @@
+package com.skillstorm.inventory_mgmt.services;
+
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import com.skillstorm.inventory_mgmt.models.Warehouse;
+import com.skillstorm.inventory_mgmt.repositories.WarehouseRepository;
+
+public class WarehouseServiceTest {
+
+    //creates a mock instance of the WarehouseRepository class
+    @Mock
+    private WarehouseRepository warehouseRepository;
+
+    //injects the WarehouseRepository mock object into the WarehouseService
+    @InjectMocks
+    private WarehouseService warehouseService;
+    private AutoCloseable closeable;
+
+    //initializes the mock objects to ensure they are ready before tests are run
+    @BeforeTest
+    public void setup() {
+        closeable = MockitoAnnotations.openMocks(this);
+    }
+
+    //closes the closeable resource to ensure that the mock objects are cleaned up properly - prevents memory leaks
+    @AfterTest
+    public void teardown() throws Exception{
+        closeable.close();
+    }
+
+    // Tests if the mocked warehouse repo successfully return expected response
+    // after service calls findAll() with expected values
+    @Test
+    public void findAllWarehousesTest() {
+        //Given
+        List<Warehouse> expectedWarehouses = Arrays.asList(new Warehouse(), new Warehouse());
+
+        //When (stubbing)
+        when(warehouseRepository.findAll())
+        
+        //Then
+        .thenReturn(expectedWarehouses);
+
+        List<Warehouse> response = warehouseService.findAll();
+
+        Assert.assertEquals(response, expectedWarehouses);
+    }
+
+    // Tests if the mocked warehouse repo successfully return expected response
+    // after service calls findById() for a warehouse id that does exist 
+    @Test
+    public void findWarehouseByIdTest() {
+        int warehouseId = 1;
+        Warehouse inputWarehouse = new Warehouse();
+        Optional<Warehouse> expectedWarehouse = Optional.of(inputWarehouse);
+        
+        when(warehouseRepository.findById(warehouseId))
+        .thenReturn(expectedWarehouse);
+
+        Optional<Warehouse> response = warehouseService.findById(warehouseId);
+
+        Assert.assertEquals(response, expectedWarehouse);
+    }
+
+    // Tests if the mocked warehouse repo successfully return expected response
+    // after service calls save() to create a warehouse
+    @Test
+    public void createWarehouseTest() {
+        Warehouse inputWarehouse = new Warehouse();
+
+        when(warehouseRepository.save(inputWarehouse))
+        .thenReturn(inputWarehouse);
+
+        Warehouse response = warehouseService.save(inputWarehouse);
+
+        Assert.assertEquals(response, inputWarehouse);
+    }
+
+    // Tests if the mocked warehouse repo successfully return expected response
+    // after service calls update()
+    @Test
+    public void updateWarehouseTest() {
+        Warehouse inputWarehouse = new Warehouse(1, 111, "testName");
+        when(warehouseRepository.save(inputWarehouse))
+        .thenReturn(inputWarehouse);
+
+        when(warehouseRepository.existsById(inputWarehouse.getWarehouseId()))
+        .thenReturn(true);
+        
+        System.out.println(inputWarehouse);
+
+        int response = warehouseService.update(inputWarehouse.getWarehouseId(), inputWarehouse);
+
+        Assert.assertEquals(response, inputWarehouse.getWarehouseId());
+    }
+
+    // Tests if the mocked warehouse repo successfully return expected EXCEPTION
+    // after service calls update()
+    @Test
+    public void updateWarehouseInvalidTest() {
+        Warehouse inputWarehouse = new Warehouse(1, 111, "testName");
+        when(warehouseRepository.save(inputWarehouse))
+        .thenReturn(inputWarehouse);
+
+        when(warehouseRepository.existsById(2))
+        .thenReturn(false);
+        
+        Assert.assertThrows(NoSuchElementException.class, () -> {
+            warehouseService.update(2, inputWarehouse);
+        });
+    }
+
+    // Tests if the mocked warehouse repo successfully deletes product with matching id after service calls deleteById()
+    @Test
+    public void deleteWarehouseTest() {
+        Warehouse inputWarehouse = new Warehouse(1, 111, "testName");
+        Optional<Warehouse> expectedWarehouse = Optional.of(inputWarehouse);
+
+        when(warehouseRepository.findById(inputWarehouse.getWarehouseId()))
+        .thenReturn(expectedWarehouse);
+        
+        warehouseRepository.deleteById(inputWarehouse.getWarehouseId());
+        verify(warehouseRepository, times(1)).deleteById(inputWarehouse.getWarehouseId());
+    }
+}
