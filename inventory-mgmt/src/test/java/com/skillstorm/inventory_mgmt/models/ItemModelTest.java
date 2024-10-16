d81e1f08345535a4069673c272c93883ba281c5e
basedmarv <sabasmarvinn@gmail.com>
2024-10-16 13:49:05 -0400

    Improve code coverage to 100% for ItemModelTest

--- inventory-mgmt/src/test/java/com/skillstorm/inventory_mgmt/models/ItemModelTest.java
+++ inventory-mgmt/src/test/java/com/skillstorm/inventory_mgmt/models/ItemModelTest.java
@@ -7,16 +7,19 @@ import org.testng.annotations.Test;
 
 public class ItemModelTest {
     private Item item;
+    private Warehouse testWarehouse;
 
     @BeforeMethod
     public void setUp() {
         item = new Item();
+        testWarehouse = new Warehouse(1, 100, "testWarehouse");
 
         item.setItemId(1);
         item.setName("testName");
         item.setDescription("testDescription");
         item.setPrice(20);
         item.setQuantity(12);
+        item.setWarehouse(testWarehouse);
     }
     // Tear down after tests
     @AfterTest
@@ -25,7 +28,7 @@ public class ItemModelTest {
     }
 
     // The below methods assert that the item's getters & setters are successfully retrieved and set
-    // for the following properties: id, name, description, price, & quantity
+    // for the following properties: id, name, description, price, quantity, & warehouse
     @Test
     public void idGetterSetter() {
         Assert.assertEquals(item.getItemId(), 1);
@@ -52,5 +55,20 @@ public class ItemModelTest {
         Assert.assertEquals(item.getPrice(), 20);
         item.setPrice(40);
         Assert.assertEquals(item.getPrice(), 40);
+    }
+
+    @Test
+    public void quantityGetterSetter() {
+        Assert.assertEquals(item.getQuantity(), 12);
+        item.setQuantity(100);
+        Assert.assertEquals(item.getQuantity(), 100);
+    }
+
+    @Test
+    public void warehouseGetterSetter() {
+        Assert.assertEquals(item.getWarehouse(), testWarehouse);
+        Warehouse newWarehouse = new Warehouse(2, 200, "newWarehouse");
+        item.setWarehouse(newWarehouse);
+        Assert.assertEquals(item.getWarehouse(), newWarehouse);
     }
 }
