fa3217b5a0958953d837cb5f36ab6a9e27ce57a9
basedmarv <sabasmarvinn@gmail.com>
2024-10-23 22:05:27 -0400

    Add sort functionality to Items

--- frontend/src/pages/Items/Items.jsx
+++ frontend/src/pages/Items/Items.jsx
@@ -1,9 +1,29 @@
 import { Link } from "react-router-dom";
 import ItemCard from "../../components/itemCard/ItemCard";
 import styles from "./Items.module.css";
+import { useState,useEffect } from "react";
 
-const Items = (props) => {
-  if (props.items.length === 0)
+const Items = ({items}) => {
+  const [sortedItems, setSortedItems] = useState([]);
+
+  useEffect(() => {
+    setSortedItems([...items]);
+  }, [items]);
+
+  const handleSort = (evt) => {
+    const {value} = evt.target
+    if(value == "itemName"){
+      setSortedItems([...sortedItems].sort((a, b) => a.name.localeCompare(b.name)));
+    }
+    if(value == "price"){
+      setSortedItems([...sortedItems].sort((a,b) => a.price - b.price));
+    }
+    if(value == "quantity"){
+      setSortedItems([...sortedItems].sort((a, b) => a.quantity - b.quantity));
+    }
+  }
+
+  if (items.length === 0)
     return (
       <main>
         <h2>
@@ -17,7 +37,15 @@ const Items = (props) => {
     return(
       <>
       <main className={styles.itemMain}>
+      <div className="selectContainer">
       <Link to="/items/item/add" className="linkTag">Add Item</Link>
+          <select name="sort" id="sort" onChange={handleSort} className={styles.itemTable}>
+            <option value="sortBy">Sort By:</option>
+            <option value="itemName">Name</option>
+            <option value="price">Price</option>
+            <option value="quantity">Quantity</option>
+          </select>
+      </div>
         <table className={styles.itemTable}>
           <thead>
             <tr>
@@ -30,7 +58,7 @@ const Items = (props) => {
             </tr>
           </thead>
           <tbody>
-            {props.items.map(item => (
+            {sortedItems.map(item => (
               <ItemCard key={item.itemId} item={item}/>
             ))}
           </tbody>
