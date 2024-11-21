0e58714c4ac68bfc91f57f98cbaf5c2ea0ae9687
basedmarv <sabasmarvinn@gmail.com>
2024-11-21 13:50:39 -0500

    Update Navigator

--- cucumber-selenium-tests/src/test/java/com/skillstorm/Utilities/Navigator.java
+++ cucumber-selenium-tests/src/test/java/com/skillstorm/Utilities/Navigator.java
@@ -9,10 +9,12 @@ public class Navigator {
     // #region Static fields
     public static final String PGNAME_HOME = "Home";
     public static final String PGNAME_WAREHOUSES = "Warehouses";
+    public static final String PGNAME_ITEMS = "Items";
 
     public static final String URL = System.getProperty("frontendUrl", "http://localhost:5173");
     public static final String URL_HOME = URL;
     public static final String URL_WAREHOUSES = URL + "/warehouses";
+    public static final String URL_ITEMS = URL + "/items";
     // #endregion
 
     Map<String, String> pageUrlMap = new HashMap<>();
@@ -21,6 +23,7 @@ public class Navigator {
     public Navigator(WebDriver driver) {
         this.driver = driver;
         pageUrlMap.put(PGNAME_WAREHOUSES, URL_WAREHOUSES);
+        pageUrlMap.put(PGNAME_ITEMS, URL_ITEMS);
     }
 
     /**
@@ -35,6 +38,9 @@ public class Navigator {
             case PGNAME_WAREHOUSES:
                 navigateToWarehouses();
                 break;
+            case PGNAME_ITEMS:
+                navigateToItems();
+                break;
             default:
                 throw new IllegalArgumentException("Page '" + pageName + "' does not exist.");
         }
@@ -43,7 +49,12 @@ public class Navigator {
     public String getURL(String pageName) {
         return pageUrlMap.get(pageName);
     }
+
     private void navigateToWarehouses() {
         driver.get(URL_WAREHOUSES);
+    }
+
+    private void navigateToItems() {
+        driver.get(URL_ITEMS);
     }
 }
