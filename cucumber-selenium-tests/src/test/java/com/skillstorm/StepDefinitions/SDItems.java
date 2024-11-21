e38fbe9dfe20b747b1343b7bb68fd782c848536f
basedmarv <sabasmarvinn@gmail.com>
2024-11-21 14:33:32 -0500

    Complete items accessibility tests

--- cucumber-selenium-tests/src/test/java/com/skillstorm/StepDefinitions/SDItems.java
+++ cucumber-selenium-tests/src/test/java/com/skillstorm/StepDefinitions/SDItems.java
@@ -43,6 +43,11 @@ public class SDItems {
         navigator.navigateTo(Navigator.PGNAME_ITEMS);
     }
 
+    @And("I am on the Items Sort dropdown")
+    public void andITabToTheItemsSortDropdown(){
+        this.itemsPage.tabTo(actions, driver.findElement(By.id("sort-items")));
+    }
+
     @When("I click on the add item button")
     public void whenIClickOnTheAddItemButton(){
         itemsPage.clickButton(ItemsPage.BTN_ADDITEM_NAME);
@@ -53,6 +58,42 @@ public class SDItems {
         this.itemsPage.selectSortBy(selectOption);
     }
 
+    @When("I tab to the Items Sort dropdown")
+    public void whenITabToTheItemsSortDropdown(){
+        this.itemsPage.tabTo(actions, driver.findElement(By.id("sort-items")));
+    }
+
+    @When("I tab to click the Add Items button")
+    public void whenITabToTheAddItemsButton(){
+        this.itemsPage.tabTo(actions, driver.findElement(By.id("add-item-btn")));
+        driver.findElement(By.id("add-item-btn")).sendKeys(Keys.ENTER);
+    }
+
+    @When("I tab to Navigate to a item")
+    public void whenITabToNavigateToAItem(){
+        this.itemsPage.tabTo(actions, driver.findElement(By.id("get-item-link")));
+        driver.findElement(By.id("get-item-link")).sendKeys(Keys.ENTER);
+    }
+
+    @When("I press the arrow key down two times and up one time on the Items Sort dropdown")
+    public void whenIPressTheArrowKeyDownOneTimeOnTheItemsSortDropdown(){
+        this.itemsPage.pressArrowKeyNTimes(actions, Keys.ARROW_DOWN, 2);
+        this.itemsPage.pressArrowKeyNTimes(actions, Keys.ARROW_UP, 1);
+    }
+
+    @Then("the items sort dropdown will be focused")
+    public void thenTheItemsSortDropdownWillBeFocused() {
+        WebElement itemsSortDropdown = itemsPage.getItemsSortDropdown();
+        WebElement focusedElement = driver.switchTo().activeElement();
+
+        Assert.assertEquals(itemsSortDropdown, focusedElement);
+    }
+
+    @Then("I can see the Update Item button")
+    public void thenICanSeeTheUpdateItemButton(){
+        Assert.assertTrue(driver.findElement(By.id("update-item-btn")).isDisplayed());
+    }
+
     @Then("I can see the add item form modal")
     public void thenICanSeeTheAddItemFormModal(){
         Assert.assertTrue(driver.findElement(By.id("add-item-form-modal")).isDisplayed());
@@ -61,6 +102,14 @@ public class SDItems {
     @Then("I can see the item list ordered by {string}")
     public void thenIShouldSeeTheOrderedItemList(String selectOption) {
         Assert.assertTrue(itemsPage.isOrdered(selectOption));
+    }
+
+    @Then("I should see the second item in the Items Sort dropdown highlighted")
+    public void thenIShouldSeeTheSecondItemInTheItemSortDropdownHighlighted() {
+        WebElement itemsSortDropdown = itemsPage.getItemsSortDropdown();
+        WebElement focusedElement = driver.switchTo().activeElement();
+
+        Assert.assertEquals(itemsSortDropdown, focusedElement);
     }
 
     @Then("I can see the Add Item button")
---
+++ cucumber-selenium-tests/src/test/resources/com/skillstorm/items-accessibility.feature
@@ -0,0 +1,24 @@
+@items-accessibility
+Feature: Items Accessibility
+
+    Scenario: Item Sort Dropdowns are tab-accessible
+        Given I am on the Items page
+        When I tab to the Items Sort dropdown
+        Then the items sort dropdown will be focused
+
+    Scenario: Item Sort Dropdown items are navigable via the up and down arrow keys
+        Given I am on the Items page
+        And I am on the Items Sort dropdown
+        When I press the arrow key down two times and up one time on the Items Sort dropdown
+        Then I should see the second item in the Items Sort dropdown highlighted
+        And I can see the item list ordered by "Price"
+
+    Scenario: Navigate by keyboard to Add Item
+        Given I am on the Items page
+        When I tab to click the Add Items button
+        Then I can see the add item form modal
+
+    Scenario: Navigate by keyboard to Get Item
+        Given I am on the Items page
+        When I tab to Navigate to a item
+        Then I can see the Update Item button
